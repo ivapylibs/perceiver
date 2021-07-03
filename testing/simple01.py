a6af1b183c2317245dc55c9407a8ff6389a0e7d4
Patricio A. Vela <pvela@gatech.edu>
2021-07-03 18:18:02 -0400

    Adding simple test script stub.

---
+++ testing/simple01.py
@@ -0,0 +1,62 @@
+#=============================== simple01 ================================
+#
+# @brief    Code to test out the simple perceiver for a fairly
+#           contrived scenario.
+#
+# Here the image is not binary, but grayscale.  It gets thresholded to
+# generate a single region.  That region is processed to generate a
+# track point. The entire implementation is constructed and
+# encapsulated within a simple perceiver.
+#
+# The code below
+#
+# > simple01
+# 
+# runs the script.  
+#
+#=============================== simple01 ================================
+
+# 
+# @file     trackTri01.m
+#
+# @author   Patricio A. Vela,   pvela@gatech.edu
+# @date     2020/11/10 [created]
+#
+#!NOTE:
+#!  Indent is set to 2 spaces.
+#!  Tab is set to 4 spaces with conversion to spaces.
+#
+# @quit
+#=============================== simple01 ================================
+
+
+#==[0] Create environment. Import necessary libraries/packages.
+
+
+
+
+
+#==[1] Build the perceiver.
+
+#--[1.1] Create the detector instance.
+
+
+#--[1.2] and the track pointer instance.
+
+
+#--[1.3] Package up into a perceiver.
+
+
+
+#==[2] Apply perceiver to simple image.
+
+#--[2.1] Create a simple image.
+
+
+#--[2.2] Apply to simple image
+
+
+#--[2.3] Visualize the output.
+
+
+
