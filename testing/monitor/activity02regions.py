6ede69c89b5143f2eda23cc14125e26ec8b0d67e
Patricio A. Vela <pvela@gatech.edu>
2024-01-19 12:22:17 -0500

    basic proof of concept test script for region-based activity recognition.

--- testing/monitor/activity02regions.py
+++ testing/monitor/activity02regions.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-#============================== activity01regions ==============================
+#============================== activity02regions ==============================
 ## @file
 # @brief    Code to test out the basic activity monitor for a scenario with 
-#           top-left/bottom-right semantics of a tracked square in an image.
+#           cyclic tracking of a square in an image through top-left and bottom-right
+#           states.
 # 
 # The image is "grayscale"  and gets thresholded to generate a single region.
 # That region is processed to generate a track point, which is used to establish
-# activity state semantics. The implementation builds out a Monitor.
+# activity state semantics. The implementation builds out a Monitor.  The path is
+# adjusted to be cyclic/periodic so that is cycles between states 1 -> 0 -> 2 -> 0. 
 # 
 # The code below
 # 
-# > ./activity01regions
+# > ./activity02regions
 # 
 # runs the script.  
 #
 # OUTCOMES:
 #
 # The square starts off in activity region 1, leaves the region into undefined
-# space, enters activity region 2, then leaves it and stops at the path terminal 
-# point.
+# space, enters activity region 2, then leaves it and cycles back to activity region
+# 1.
 # 
 # @ingroup  TestMonitor
 # @quitf
@@ -26,13 +28,13 @@
 # @author   Patricio A. Vela,   pvela@gatech.edu
 # @date     2024/01/12 [created]
 #
-#============================== activity01regions ==============================
+#============================== activity02regions ==============================
 #
 #!NOTE:
 #!  Indent is set to 2 spaces.
 #!  Tab is set to 4 spaces with conversion to spaces.
 #
-#============================== activity01regions ==============================
+#============================== activity02regions ==============================
 
 
 import numpy as np
@@ -47,17 +49,19 @@ import detector.activity.byRegion as regact
 import perceiver.monitor as monitor
 
 
+imsize   = [100,100]
 
-
-pathPts = np.transpose(np.array([ [10,10] , [35, 35], [50, 50] ]))
-thePath = pathgen.StepLines(None, pathPts)
+pathPts  = np.transpose(np.array([ [10,10] , [40, 20], [50, 50], [20, 40] , [10, 10] ]))
+pathSpec = pathgen.CfgStepLines()
+pathSpec.isPeriodic = True
+thePath  = pathgen.StepLines(pathSpec, pathPts)
 
 thePerceiver = perbuild.buildTesterGS(10)
 
 theActivity  = regact.imageRegions()
-theActivity.initRegions([50, 50])
-theActivity.addRegionByPolygon([[ 5,  5, 15, 15],[ 5, 15, 15, 5]])
-theActivity.addRegionByPolygon([[35, 35, 45, 45],[35, 45, 45, 35]])
+theActivity.initRegions(imsize)
+theActivity.addRegionByPolygon([[ 5,  5, 15, 15], [ 5, 15, 15, 5]])
+theActivity.addRegionByPolygon([[45, 45, 55, 55], [45, 55, 55, 45]])
 
 theMonitor = monitor.Monitor(None, thePerceiver, theActivity)
 
@@ -67,13 +71,14 @@ while True:
   cpt = thePath.next()
   # @todo   IAMHERE - Code works but path doesn't update.  Need to add that.
 
-  theImage = visgen.squareInImage([100, 100], cpt, 5, [50])
+  theImage = visgen.squareInImage(imsize, cpt, 5, [50])
 
   theMonitor.process(theImage)
 
   theMonitor.displayState()
 
   display.gray(theImage)
+  display.gray(theActivity.imRegions.astype('float'),window_name="regions")
   kp = display.wait(100)
   if (kp == ord('q')):
     quit()
